fcf01bfcd09763a0b11a686f80983fa89304f614
Neo131088 <neo131088@gmail.com>
2026-01-02 16:04:55 +0100

    add

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const myKey = import.meta.env.VITE_API_KEY;
+const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 export const api = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
     accept: "application/json",
-    Authorization: myKey, 
+   Authorization: `Bearer ${TMDB_TOKEN}`,
   },
 });
